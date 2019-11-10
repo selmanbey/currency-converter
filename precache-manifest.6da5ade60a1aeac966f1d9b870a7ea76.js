403cdd30054fa3429ea6defe1c3b86b4aa724236
selmanbey <selmannur@yahoo.com>
2019-11-10 04:48:32 +0000

    Updates

---
+++ precache-manifest.6da5ade60a1aeac966f1d9b870a7ea76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c50165082ed044e85e66334385390d",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "e79c81edb19ba7abe7c9",
+    "url": "/currency-converter/static/css/main.6a35be22.chunk.css"
+  },
+  {
+    "revision": "7ffd9ddf6a1b6067a795",
+    "url": "/currency-converter/static/js/2.3e7f10c7.chunk.js"
+  },
+  {
+    "revision": "e79c81edb19ba7abe7c9",
+    "url": "/currency-converter/static/js/main.d5fb250e.chunk.js"
+  },
+  {
+    "revision": "c0d28daf08c87ac5e666",
+    "url": "/currency-converter/static/js/runtime-main.f8c82292.js"
+  }
+]);
